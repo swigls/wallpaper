6b0b9245256b41e1611f77b3e4041473beb4a450
Hyeonseung Lee <swigls1@gmail.com>
2021-12-28 21:14:54 +0900

    se

--- app.py
+++ app.py
@@ -5,33 +5,107 @@ from google_images_download import google_images_download
 import requests
 import datetime
 from bs4 import BeautifulSoup
+import re
+from dateutil.parser import parse as dtparse
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 # Acceptance rate
 #https://github.com/lixin4ever/Conference-Acceptance-Rate
-headers = {
+HEADERS = {
   "User-Agent":
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
-}
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+  "Accept-Language": "en-US,en;q=0.5",
+  }
+#HEADERS = {
+#  "User-Agent":
+#  "Chrome/70.0.3538.102",
+#}
 
+CONF_NAMES = [
+'NeurIPS',
+'ICML',
+'ICLR',
+'AAAI',
+#'ICASSP',
+#'Interspeech',
+#'CVPR',
+#'ECCV',
+#'ACL',
+#'EMNLP',
+]
 
 ### Utility functions
 def set_wallpaper(image_path):
   SPI_SETDESKWALLPAPER = 20 
   ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image_path, 0)
   
-def search_snippet(x):
+def search_snippet(x, driver):
   ph_search = 'https://www.google.com/search?q={}'
-  ph_search = ph_search + '&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBAgjECcyBQgAEIAEOgcIABBHELADOgYIABAWEB5KBAhBGABKBAhGGABQ4gRY3gdg3AloAXACeACAAXCIAcsCkgEDMC4zmAEAoAEByAEKwAEB'
-  html = requests.get(ph_search.format(x), headers=headers).text
-  soup = BeautifulSoup(html, 'html.parser')
-  for s in soup.find_all(id="res"):
-    print(s.text)
-  #summary = soup.select_one('.Uo8X3b+ span').text
+  #ph_search = ph_search + '&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBAgjECcyBQgAEIAEOgcIABBHELADOgYIABAWEB5KBAhBGABKBAhGGABQ4gRY3gdg3AloAXACeACAAXCIAcsCkgEDMC4zmAEAoAEByAEKwAEB'
+  driver.get(ph_search.format(x))
+  try: # Big
+    result = driver.find_element_by_css_selector(".Z0LcW").text
+    print('Big')
+    return result
+  except: pass
+  try: # Small where bold fonts are used for snippet
+    result = driver.find_element_by_css_selector(".hgKElc").text
+    print('Small')
+    result = result.split('<b>')[1].split('</b>')[0]
+    return result
+  except: pass
+  try: # Table
+    result = driver.find_element_by_css_selector(".iKJnec").text
+    result = result.split('<b>')[1].split('</b>')[0]
+    print('Table')
+    return result
+  except: pass
+
+    
+  '''
+  html = requests.get(ph_search.format(x), headers=HEADERS).text
+  #html = requests.get(ph_search.format(x)).text
+  #soup = BeautifulSoup(html, 'html.parser')
+  soup = BeautifulSoup(html, 'lxml')
+  #for s in soup.find_all(id="res"):
+  #  print(s)
+  for s in soup.find_all("div", class_="Z0LcW"): # Big featured snippet
+    text = re.sub("[\<].*?[\>]","",s.text)   
+    return text
+  #for s in soup.find_all("div", class_="d9FyLd"): # Small featured snippet w/ bold important phrase
+  for s in soup.find_all("span", class_="hgKElc"): # Small featured snippet w/ bold important phrase
+    try:
+      text = s.split('<b>')[1].split('</b>')[0]
+    except:
+      continue
+    #re.sub("[\<].*?[\>]","",text)  
+    return text
+  #soup = BeautifulSoup(html, 'html.parser')
+  #for s in soup.find_all("div", class_="IZ6rdc"): # Big & Small featured snippet
+    
+  #  a = 0
+  #  return None
+  for s in soup.find_all("div", class_="iKJnec"): # Table
+    print(s)
+    try:
+      text = s.split('<b>')[1].split('</b>')[0]
+    except:
+      continue
+    return text 
+  '''
+  return None
+
+def normalize_time(x):
+  x = x.replace('through','-')
+  y = x.split('-')
 
 def this_year():
   today = datetime.date.today()
   year = int(today.strftime("%Y"))
   return year
+
 
 
 ### Setup functions
@@ -43,11 +117,19 @@ class Conference():
     self.deadline = deadline
 	
 def setup():
+  options = webdriver.ChromeOptions()
+  #options.add_experimental_option('prefs', {'intl.accept_languages':'en,en_US'})
+  options.add_argument('--lang=en-US')
+  #options.add_argument('--lang=en-GB')
+  driver = webdriver.Chrome(options=options)
   # Conference wallpaper
-  conferences = init_conference_info(["NeurIPS","ICML","ICLR","AAAI","ICASSP","InterSpeech","CVPR","ECCV","ACL","EMNLP"])
+  conferences = init_conference_info(CONF_NAMES, driver)
   init_wallpaper_images(conferences)
-  
-def init_conference_info(names):
+  #
+  driver.close()
+  driver.quit()
+
+def init_conference_info(names, driver):
   '''
   # Initial crawling w/ ai-deadlines
   info_hub_url = 'http://aideadlin.es/?sub=ML,CV,NLP,SP'
@@ -59,12 +141,17 @@ def init_conference_info(names):
   pass
   '''
   ph_date = '{} date'
-  ph_place = 'where is {}'
+  ph_place = '{} place'
   ph_deadline = '{} paper submission deadline'
-  q_date = ph_date.format(names[0] +" "+ str(this_year()))
-  print(q_date)
-  a = search_snippet(q_date)
-   
+
+  for name in names:
+    for year in [this_year(), this_year()+1]:
+      q_date = ph_date.format(name +" "+ str(year))
+      a = search_snippet(q_date, driver)
+      print(q_date, a)
+      q_place = ph_place.format(name +" "+str(year))
+      b = search_snippet(q_place, driver)
+      print(q_place, b)
   return []
 
 def init_wallpaper_images(conferences):
